b3aa98ab4311c3a4df5e2c3acf64e64550a64bbc
Merve Bulut <47273479+merve-bulut@users.noreply.github.com>
2025-03-06 21:45:45 +0300

    Initial Commit

---
+++ RandomColor/RandomColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomColorApp.swift
+//  RandomColor
+//
+//  Created by Merve Bulut on 6.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct RandomColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
